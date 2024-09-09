0f966a137d781a3b7a729facfc797271608906be
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-09-09 07:59:34 +0200

    Add a spinner

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,6 +1,6 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { createIssueSchema } from '../../validationSchemas.js';
+import { createIssueSchema } from '../../validationSchemas';
 
 
 export async function POST(request: NextRequest) {
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
 import ErrorMessage from '@/components/ErrorMessage';
+import Spinner from '@/components/Spinner';
 
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
   ssr: false
@@ -23,6 +24,7 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema)
   });
   const [ error, setError ] = useState('');
+  const [ isSubmitting, setSubmitting ] = useState(false);
 
   return (
     <div className='max-w-xl'>
@@ -35,9 +37,11 @@ const NewIssuePage = () => {
       className='max-w-xl space-y-3'
       onSubmit={handleSubmit(async (data) => {
         try {
+          setSubmitting(true);
           await axios.post('/api/issues', data); 
           router.push('/issues');
         } catch (error) {
+          setSubmitting(false);
           setError('An unexpected error occured');
         }
       })}
@@ -51,7 +55,9 @@ const NewIssuePage = () => {
         />
         <ErrorMessage>{ errors.description?.message}</ErrorMessage>
         
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>
+          Submit New Issue{ isSubmitting && <Spinner />}
+        </Button>
     </form>
     </div>
   )
