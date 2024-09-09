6f6a517a74dadbece06ec4b94d8bdb755e53c349
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-09-09 08:05:22 +0200

    Refactor: Remove the inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,66 +1,72 @@
-"use client"
-import React, { useState } from 'react'
-import dynamic from 'next/dynamic';
-import axios from 'axios';
-import { Button, Callout, Text, TextField } from '@radix-ui/themes';
-import { useForm, Controller } from 'react-hook-form';
+"use client";
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+import axios from "axios";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
-import { useRouter } from 'next/navigation';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '@/app/validationSchemas';
-import { z } from 'zod';
-import ErrorMessage from '@/components/ErrorMessage';
-import Spinner from '@/components/Spinner';
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
+import ErrorMessage from "@/components/ErrorMessage";
+import Spinner from "@/components/Spinner";
 
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
-  ssr: false
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
 });
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
-    resolver: zodResolver(createIssueSchema)
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
   });
-  const [ error, setError ] = useState('');
-  const [ isSubmitting, setSubmitting ] = useState(false);
+  const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false);
+      setError("An unexpected error occured");
+    }
+  });
 
   return (
-    <div className='max-w-xl'>
-      { error && ( 
-        <Callout.Root color='red' className='mb-5'>
-          <Callout.Text>{ error }</Callout.Text>
-        </Callout.Root> 
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
       )}
-    <form 
-      className='max-w-xl space-y-3'
-      onSubmit={handleSubmit(async (data) => {
-        try {
-          setSubmitting(true);
-          await axios.post('/api/issues', data); 
-          router.push('/issues');
-        } catch (error) {
-          setSubmitting(false);
-          setError('An unexpected error occured');
-        }
-      })}
-      >
-        <TextField.Root placeholder="Title" {...register('title')} />
+      <form className="max-w-xl space-y-3" onSubmit={onSubmit}>
+        <TextField.Root placeholder="Title" {...register("title")} />
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
-        name='description'
-        control={control}
-        render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
         />
-        <ErrorMessage>{ errors.description?.message}</ErrorMessage>
-        
-        <Button disabled={isSubmitting}>
-          Submit New Issue{ isSubmitting && <Spinner />}
-        </Button>
-    </form>
-    </div>
-  )
-}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-export default NewIssuePage
+        <Button disabled={isSubmitting}>
+          Submit New Issue{isSubmitting && <Spinner />}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewIssuePage;
